d7de3c27969f8da207af0af387e438ee0131081c
EnergyboyOfficial <energyboyofficial.off@gmail.com>
2023-11-26 15:58:35 +0100

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	new file:   server.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// script.js
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('pixelCanvas');
     const ctx = canvas.getContext('2d');
@@ -5,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const clearButton = document.getElementById('clearButton');
 
     let drawing = false;
+
+    // Connect to the server via WebSocket
+    const socket = io.connect('http://localhost:3000');
 
     canvas.addEventListener('mousedown', startDrawing);
     canvas.addEventListener('mouseup', stopDrawing);
@@ -47,12 +51,16 @@ document.addEventListener('DOMContentLoaded', function () {
         ctx.font = "10px Arial";
         ctx.fillText(`${String.fromCharCode(65 + gridY)}${gridX + 1}`, pixelX + 5, pixelY + 15);
 
+        // Emit a 'draw' event to the server
+        socket.emit('draw', { x: gridX, y: gridY, color });
         savePixel(gridX, gridY, color);
     }
 
     function clearCanvas() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         localStorage.clear();
+        // Emit a 'clear' event to the server
+        socket.emit('clear');
     }
 
     function loadSavedPixels() {
@@ -80,5 +88,34 @@ document.addEventListener('DOMContentLoaded', function () {
         const savedPixels = JSON.parse(localStorage.getItem('pixels')) || [];
         savedPixels.push({ x, y, color });
         localStorage.setItem('pixels', JSON.stringify(savedPixels));
+    }
+
+    // Listen for 'draw' events from the server
+    socket.on('draw', (data) => {
+        const { x, y, color } = data;
+        drawOnCanvas(x, y, color);
+        savePixel(x, y, color);
+    });
+
+    // Listen for 'clear' events from the server
+    socket.on('clear', () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        localStorage.clear();
+    });
+
+    function drawOnCanvas(x, y, color) {
+        const pixelSize = 20;
+        const pixelX = x * pixelSize;
+        const pixelY = y * pixelSize;
+
+        ctx.fillStyle = color;
+        ctx.fillRect(pixelX, pixelY, pixelSize, pixelSize);
+
+        // Draw borders and labels
+        ctx.strokeStyle = "#ccc";
+        ctx.strokeRect(pixelX, pixelY, pixelSize, pixelSize);
+        ctx.fillStyle = "#555";
+        ctx.font = "10px Arial";
+        ctx.fillText(`${String.fromCharCode(65 + y)}${x + 1}`, pixelX + 5, pixelY + 15);
     }
 });
---
+++ server.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('draw', (data) => {
+        // Broadcast the draw event to all connected clients
+        io.emit('draw', data);
+    });
+
+    socket.on('clear', () => {
+        // Broadcast the clear event to all connected clients
+        io.emit('clear');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
